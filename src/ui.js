f05dfecc917984e9815abe31f238475ef68f9a4f
Christian Valese <valesechristian@icloud.com>
2026-05-19 13:38:40 +0200

    Optimize header space: hide brand mark and scale top stats to full width, bump to 1.5.5

--- src/ui.js
+++ src/ui.js
@@ -127,7 +127,7 @@ import {
 import { exportState, importState, resetState, saveState } from "./store.js";
 import { escapeHtml, percent, clamp, rarityClass, compactTime, casePoolPreview, formatPercent, parseTransformX, dropFeedHeadline, upgradeBranch, iconMarkup, profileAvatarMarkup, tabIcon, hashText, upgradeEffectText, itemCard, statTile, casePriceLabel, reelDisplayItem, PROFILE_ICON_OPTIONS, NAV_TABS, ADMIN_STORAGE_KEY, ADMIN_USER_ID, ADMIN_PASSWORD_HASH, ADMIN_ONLY_ACTIONS, LOGIN_GATE_ACTIONS, TAB_GROUPS, TAB_PARENT } from "./ui/components/uiElements.js";
 
-const GAME_VERSION = "v1.5.4";
+const GAME_VERSION = "v1.5.5";
 
 export class CaseOpenerUI {
   constructor(root, state, skinData, metadata) {
